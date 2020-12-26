9121d1b5c6c5fa6d51d164eae711ef3a3079e1b7
takumiwada <takumiwada129@gmail.com>
2020-12-27 02:45:47 +0900

    シーンマネージャー追加

---
+++ Assets/SceneManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+
+#region Singleton
+    static public SceneManager instance;
+
+    void  Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+#endregion
+
+    public enum SceneStateEnum
+    {
+        eTitle,
+        eSelect,
+        eMain,
+    }
+    SceneStateEnum eSceneState;
+
+    void Start()
+    {
+        eSceneState = SceneStateEnum.eTitle;
+    }
+    void Update()
+    {
+        switch(eSceneState)
+        {
+            case SceneStateEnum.eTitle:
+            break;
+            case SceneStateEnum.eSelect:
+            break;
+            case SceneStateEnum.eMain:
+            break;
+        }
+    }
+}
